c8bb044cb037f70b15387c30f2bc0575f6db20ca
nijamhamy <amnijam60@gmail.com>
2026-09-16 00:39:02 +0530

    Secure supabase keys using environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://yhdgzxgfwiremmicofpz.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_szf4KBKWx_Vfr9JSIPquDw_v42BSg2d'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(supabaseUrl, supabaseKey)
